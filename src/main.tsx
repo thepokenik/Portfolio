9fe0f325a3936d0c96e75d90b3072676fe8c330a
thepokenik <nikolaspedro.np@gmail.com>
2024-09-09 17:01:22 -0300

    Refactor main.tsx to add routing and toast functionality

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+// Routes
+import App from './App.tsx';
+
+// Router
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// Toast
+import { Toaster } from './components/ui/toaster.tsx';
+
+const BrowserRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+      <RouterProvider router={BrowserRouter} />
+      <Toaster />
+  </React.StrictMode>,
 )
