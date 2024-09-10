b8cf85d9575cf4e3263e8191dccc32dbc7825c9a
thepokenik <nikolaspedro.np@gmail.com>
2024-09-10 16:36:01 -0300

    Feat: Add motion animation and improve responsiveness

--- src/pages/Portfolio/Portfolio.tsx
+++ src/pages/Portfolio/Portfolio.tsx
@@ -1,52 +1,105 @@
-import { Link } from "react-router-dom";
-import { BrainCog, Github, Linkedin, Instagram } from 'lucide-react';
+import { motion } from 'framer-motion';
+import { BrainCog, Github, Linkedin, Instagram, ArrowUpRight } from 'lucide-react';
 import { Button } from "@/components/ui/button";
+import { useEffect, useState } from 'react';
+
+const animationProps = {
+    initial: { opacity: 0, scale: 0.5 },
+    animate: { opacity: 1, scale: 1 },
+    transition: {
+        duration: 1,
+        delay: 2,
+        ease: [0, 0.71, 0.2, 1.01]
+    }
+};
 
 export default function Portfolio() {
+    const handleRefresh = () => {
+        window.location.reload();
+    };
+
+    const [step, setStep] = useState(0);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            if (step < 3) setStep(step + 1);
+        }, 500);
+
+        return () => clearTimeout(timer);
+    }, [step]);
+
+    const variants = {
+        initial: { scale: 1.2, x: -100, y: 100, transition: { duration: 0.5 } },
+        shrink: { scale: 1, transition: { duration: 0.5 } },
+        center: { x: 0, y: 0, scale: 1, transition: { duration: 0.5 } },
+    };
+
     return (
         <main className="grid grid-cols-3 auto-cols-max gap-4">
-            <div className="grid grid-rows-2 gap-4 col-span-2 h-full ">
-                <div className="grid grid-cols-3 gap-4 h-full ">
-                    <div className="bg-primary/70 p-6 rounded-lg text-primary-foreground col-span-2">
+            <div className="grid grid-rows-3 gap-4 col-span-2 h-full ">
+                <div className="grid grid-cols-3 gap-4 row-span-2">
+                    <motion.div
+                        {...animationProps}
+                        className="bg-primary/70 p-6 rounded-lg text-primary-foreground col-span-2">
                         <BrainCog width={100} height={100} />
                         Nikolas
-                    </div>
-
-                    <div className="rounded-lg overflow-hidden ">
+                    </motion.div>
+                    <motion.div
+                        variants={variants}
+                        initial="initial"
+                        animate={step === 0 || step === 1 ? "" : step === 2 ? "shrink" : "center"}
+                        className="flex h-full w-full items-center justify-cente"
+                    >
                         <img
-                            src="https://40ton.net/wp-content/uploads/2023/02/man_tgx_2024.jpg"
-                            alt="Portrait of Julia Huang"
-                            width={400}
-                            height={400}
-                            className="w-full h-full object-cover"
+                            src="https://avatars.githubusercontent.com/u/118072254?s=400"
+                            alt="Portrait of Nikolas Melo's Github"
+                            className="w-[450px] h-full rounded-lg object-cover"
                         />
-                    </div>
+                    </motion.div>
                 </div>
                 <div className="grid grid-cols-2 gap-4 h-full">
-                    <div className="bg-primary/70 p-6 rounded-lg col-span-1">
+                    <motion.div
+                        {...animationProps}
+                        className="bg-primary/70 p-6 rounded-lg col-span-1">
                         <p className="text-sm">
 
                         </p>
-                    </div>
+                    </motion.div>
 
-                    <div className="bg-primary p-6 rounded-lg flex flex-col justify-between col-span-1">
-                        <h3 className="text-2xl font-bold mb-4">Contact me</h3>
-                        <Link to="/" className="self-end">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14 5l7 7m0 0l-7 7m7-7H3" />
-                            </svg>
-                        </Link>
-                    </div>
+                    <motion.div
+                        className="bg-primary p-6 rounded-lg flex flex-col justify-between col-span-1"
+                        {...animationProps}
+                    >
+                        <div className='flex justify-between items-center'>
+                            <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">Have some questions?</h4>
+                            <Button onClick={handleRefresh} size="icon" >
+                                <ArrowUpRight />
+                            </Button>
+                        </div>
+                        <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">Contact me</h2>
+                    </motion.div>
                 </div>
             </div>
             <div className="grid grid-rows-4 gap-4 col-span-1 h-full">
-                <div className="bg-primary/70 p-6 rounded-lg row-span-4">
+                <motion.div
+                    {...animationProps}
+                    className="bg-primary/70 p-6 rounded-lg row-span-4">
                     <p className="text-sm">
 
                     </p>
-                </div>
+                </motion.div>
 
-                <div className="bg-primary/70 p-6 rounded-lg flex flex-col justify-end">
+                <motion.div
+                    initial={{ opacity: 0, scaleX: 0 }}
+                    animate={{ opacity: 1, scaleX: 1 }}
+                    transition={{
+                        duration: 1,
+                        delay: 2,
+                        ease: [0, 0.7, 0.2, 1]
+                    }}
+                    style={{ transformOrigin: "center" }}
+                    className="bg-primary/70 p-6 rounded-lg flex flex-col justify-end"
+                >
                     <div className="flex justify-between">
                         <Button variant="outline" asChild>
                             <a href="https://www.linkedin.com/in/nikolas-melo-5743b1258/">
@@ -64,7 +117,7 @@ export default function Portfolio() {
                             </a>
                         </Button>
                     </div>
-                </div>
+                </motion.div>
             </div>
         </main>
     )
