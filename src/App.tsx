f161e17280283a1122ae333d26bda67b25e50424
thepokenik <nikolaspedro.np@gmail.com>
2024-09-10 16:35:11 -0300

    Refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import Navbar from './pages/Components/Navbar/Navbar';
 import Portfolio from './pages/Portfolio/Portfolio';
+import './App.css';
 
 function App() {
   return (
-    <div className="bg-background min-h-screen flex flex-col p-8">
+    <div className="bg-background h-screen flex flex-col p-8">
       <Navbar />
       <div className="flex-grow flex">
         <Portfolio />
