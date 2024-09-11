df9a9a3bba1f94fd06324dd5934e77504f9afe8a
Nikolas Melo <nikolaspedro.np@gmail.com>
2024-09-11 18:11:41 -0300

    Refactor styling in App and index.css

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="bg-background h-screen flex flex-col p-8">
+    <div className="h-screen flex flex-col p-8">
       <Navbar />
       <div className="flex-grow flex">
         <Portfolio />
